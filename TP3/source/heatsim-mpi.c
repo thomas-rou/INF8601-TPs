cabf117c23f0f177d114d88c16374b704a259a6c
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-12-05 17:47:38 -0500

    Implement 2D Cartesian communicator and implement grid data sending in heatsim_mpi

--- TP3/source/heatsim-mpi.c
+++ TP3/source/heatsim-mpi.c
@@ -10,6 +10,46 @@ int heatsim_init(heatsim_t* heatsim, unsigned int dim_x, unsigned int dim_y) {
      *       Le communicateur doit être périodique. Le communicateur
      *       cartésien est périodique en X et Y.
      */
+    MPI_Status status;
+    // Create a 2D cartesian communicator
+    if (MPI_Cart_create(MPI_COMM_WORLD, 2, (int[]){dim_x, dim_y}, (int[]){1, 1}, 0, &heatsim->communicator) != MPI_SUCCESS) {
+        LOG_ERROR("MPI_Cart_create failed");
+        goto fail_exit;
+    }
+
+    // Get the rank of the current communicator
+    if(MPI_Comm_rank(heatsim->communicator, &heatsim->rank) == MPI_ERR_COMM) {
+        LOG_ERROR("MPI_Comm_rank failed");
+        goto fail_exit;
+    }
+
+    // Get the number of processes in the communicator
+    if(MPI_Comm_size(heatsim->communicator, &heatsim->rank_count) != MPI_SUCCESS) {
+        LOG_ERROR("MPI_Comm_size failed");
+        goto fail_exit;
+    }
+
+    // Get the coord of the current process
+    if(MPI_Cart_coords(heatsim->communicator, heatsim->rank, 2, heatsim->coordinates) != MPI_SUCCESS) {
+        LOG_ERROR("MPI_Cart_coords failed");
+        goto fail_exit;
+    }
+
+    // Get the rank of the neighbors communicators
+    // Get north and south neighbors
+    if(MPI_Cart_shift(heatsim->communicator, 0, 1, &heatsim->rank_north_peer, &heatsim->rank_south_peer) != MPI_SUCCESS) {
+        LOG_ERROR("MPI_Cart_shift failed");
+        goto fail_exit;
+    }
+
+    // Get west and east neighbors
+    if(MPI_Cart_shift(heatsim->communicator, 1, 1, &heatsim->rank_west_peer, &heatsim->rank_east_peer) != MPI_SUCCESS) {
+        LOG_ERROR("MPI_Cart_shift failed");
+        goto fail_exit;
+    }
+
+    return 0;
+
 
 fail_exit:
     return -1;
@@ -28,6 +68,66 @@ int heatsim_send_grids(heatsim_t* heatsim, cart2d_t* cart) {
      *
      *       Utilisez `cart2d_get_grid` pour obtenir la `grid` à une coordonnée.
      */
+    for (int destination_rank = 1; destination_rank < heatsim->rank_count; destination_rank++) {
+        grid_t* grid = cart2d_get_grid(cart, heatsim->coordinates[0], heatsim->coordinates[1]);
+        if (grid == NULL) {
+            LOG_ERROR("failed to get grid at (%d, %d) (rank 0)", heatsim->coordinates[0],
+                      heatsim->coordinates[1]);
+            goto fail_exit;
+        }
+
+        MPI_Request request;
+        MPI_Status status;
+
+        // Send the width, height and padding of the grid using pointer to width (struct have height and padding after width)
+        if(MPI_Isend(&grid->width, 3, MPI_UNSIGNED, destination_rank, 0, heatsim->communicator, &request) != MPI_SUCCESS) {
+            LOG_ERROR("MPI_Isend failed to send grid info to rank %d", destination_rank);
+            goto fail_exit;
+        }
+
+        if(MPI_Wait(&request, &status) != MPI_SUCCESS) {
+            LOG_ERROR("MPI_Wait failed to wait for grid info to rank %d", destination_rank);
+            goto fail_exit;
+        }
+
+        // Create a sendable datatype for the grid
+        MPI_Datatype ElementType[] = {MPI_DOUBLE};
+        int elementBlockSize[] = {grid->width_padded * grid->height_padded};
+        MPI_Aint elementDisplacement[] = {0};
+        MPI_Datatype datatype;
+
+        // Create the datatype
+        if(MPI_Type_create_struct(1, elementBlockSize, elementDisplacement, ElementType, &datatype) != MPI_SUCCESS) {
+            LOG_ERROR("MPI_Type_create_struct failed to create datatype for grid");
+            goto fail_exit;
+        }
+
+        // Commit the datatype to allow it to be used in MPI_Isend
+        if(MPI_Type_commit(&datatype) != MPI_SUCCESS) {
+            LOG_ERROR("MPI_Type_commit failed to commit datatype for grid");
+            goto fail_exit;
+        }
+
+        // Send the grid data with the custom type
+        if(MPI_Isend(grid->data, 1, datatype, destination_rank, 0, heatsim->communicator, &request) != MPI_SUCCESS) {
+            LOG_ERROR("MPI_Isend failed to send grid data to rank %d", destination_rank);
+            goto fail_exit;
+        }
+
+        if(MPI_Wait(&request, &status) != MPI_SUCCESS) {
+            LOG_ERROR("MPI_Wait failed to wait for grid data to rank %d", destination_rank);
+            goto fail_exit;
+        }
+
+
+        // Free the datatype
+        if(MPI_Type_free(&datatype) != MPI_SUCCESS) {
+            LOG_ERROR("MPI_Type_free failed to free datatype for grid");
+            goto fail_exit;
+        }
+    }
+
+    return 0;
 
 fail_exit:
     return -1;
